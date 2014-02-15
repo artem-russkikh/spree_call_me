a8b1a4e5d1fd329a87a0743a5aa89e35b7b1367b
Русских Артем <rusartx@gmail.com>
2014-02-16 03:29:01 +0600

    mailer

--- app/controllers/spree/call_backs_controller.rb
+++ app/controllers/spree/call_backs_controller.rb
@@ -8,18 +8,19 @@ class Spree::CallBacksController < ApplicationController
 
   def create
     @spree_admin_callback = Spree::CallBack.new(spree_admin_callback_params)
-    Spree::CallBackMailer.notificate.deliver if @spree_admin_callback.save
+    @spree_admin_callback.save
+    Spree::CallBackMailer.notificate.deliver(@spree_admin_callback) if Spree::Config.enable_mail_delivery
     render template: 'spree/call_backs/create'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_spree_admin_callback
-      @spree_admin_callback = Spree::CallBack.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_spree_admin_callback
+    @spree_admin_callback = Spree::CallBack.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def spree_admin_callback_params
-      params.require(:call_back).permit(:name, :phone, :comment)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def spree_admin_callback_params
+    params.require(:call_back).permit(:name, :phone, :comment)
+  end
 end
--- app/mailers/spree/call_back_mailer.rb
+++ app/mailers/spree/call_back_mailer.rb
@@ -3,6 +3,6 @@ class Spree::CallBackMailer < ActionMailer::Base
 
   def notificate(object)
     @object = object
-    mail to: Spree::User.admin.first.email if Spree::Config.enable_mail_delivery
+    mail to: Spree::Config.preferred_mail_bcc
   end
 end
